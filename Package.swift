af177130032deb40bdeaefff197931328c532fd1
Andrew Barba <abarba.77@gmail.com>
2017-11-09 11:23:31 -0500

    Add SPM support

---
+++ Package.swift
@@ -0,0 +1,14 @@
+//
+//  Package.swift
+//  Fritz
+//
+//  Created by Andrew Barba on 8/9/17.
+//  Copyright © 2017 Fritz Labs Incorporated. All rights reserved.
+//
+
+import PackageDescription
+
+let package = Package(
+    name: "Fritz",
+    swiftLanguageVersions: [4]
+)
