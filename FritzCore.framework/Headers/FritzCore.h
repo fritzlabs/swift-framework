2d5442d96cf8be6ab8e3530f7fe63512b6bfe09f
Chris Kelly <ghop02@gmail.com>
2018-06-11 01:11:31 -0400

    Add all fritz frameworks

---
+++ FritzCore.framework/Headers/FritzCore.h
@@ -0,0 +1,19 @@
+//
+//  FritzCore.h
+//  FritzCore
+//
+//  Created by Christopher Kelly on 6/8/18.
+//  Copyright © 2018 Fritz Labs Incorporated. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for FritzCore.
+FOUNDATION_EXPORT double FritzCoreVersionNumber;
+
+//! Project version string for FritzCore.
+FOUNDATION_EXPORT const unsigned char FritzCoreVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <FritzCore/PublicHeader.h>
+
+
---
+++ FritzVision.framework/Headers/FritzVision.h
@@ -0,0 +1,19 @@
+//
+//  FritzVision.h
+//  FritzVision
+//
+//  Created by Christopher Kelly on 6/8/18.
+//  Copyright © 2018 Fritz Labs Incorporated. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for FritzVision.
+FOUNDATION_EXPORT double FritzVisionVersionNumber;
+
+//! Project version string for FritzVision.
+FOUNDATION_EXPORT const unsigned char FritzVisionVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <FritzVision/PublicHeader.h>
+
+
