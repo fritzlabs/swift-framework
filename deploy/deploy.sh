ddc2d33a3a3bfce080e673c8b03378d830650491
Dan Abdinoor <abdinoor@gmail.com>
2018-06-28 13:49:23 -0400

    update the regex and make the uploads quiet

--- deploy/deploy.sh
+++ deploy/deploy.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
 
-S3_BUCKET=fritz-docs
+S3_BUCKET=abdinoor-test
+# S3_BUCKET=fritz-docs
 
-echo Pushing docs folder $CI_BRANCH/
-aws s3 cp /docs/ s3://${S3_BUCKET}/iOS/${CI_BRANCH}/  --recursive
+echo Pushing docs folder $CI_BRANCH
+aws s3 cp /docs/ s3://${S3_BUCKET}/iOS/${CI_BRANCH}/ --recursive --quiet
 
 # write the redirects to this version
 echo Pushing redirect
 sed -e "s/CI_BRANCH/$CI_BRANCH/g" deploy/redirect.html > /tmp/index.html
-aws s3 cp /tmp/index.html s3://${S3_BUCKET}/iOS/index.html
-aws s3 cp /tmp/index.html s3://${S3_BUCKET}/iOS/latest/index.html
+aws s3 cp /tmp/index.html s3://${S3_BUCKET}/iOS/index.html --quiet
+aws s3 cp /tmp/index.html s3://${S3_BUCKET}/iOS/latest/index.html --quiet
