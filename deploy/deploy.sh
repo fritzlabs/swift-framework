56b15a9040abecb9ba10bb4de02038ff6ebe0724
Dan Abdinoor <abdinoor@gmail.com>
2018-07-19 17:50:10 -0400

    Generate Jazzy docs on tagged deploy

--- deploy/deploy.sh
+++ deploy/deploy.sh
@@ -3,10 +3,10 @@
 S3_BUCKET=fritz-docs
 
 echo Pushing docs folder $CI_BRANCH
-aws s3 cp /docs/ s3://${S3_BUCKET}/iOS/${CI_BRANCH}/ --recursive --quiet
+aws s3 cp /build/ s3://${S3_BUCKET}/iOS/${CI_BRANCH}/ --recursive --only-show-errors
 
-# write the redirects to this version
+# # write the redirects to this version
 echo Pushing redirect
 sed -e "s/CI_BRANCH/$CI_BRANCH/g" deploy/redirect.html > /tmp/index.html
-aws s3 cp /tmp/index.html s3://${S3_BUCKET}/iOS/index.html --quiet
-aws s3 cp /tmp/index.html s3://${S3_BUCKET}/iOS/latest/index.html --quiet
+aws s3 cp /tmp/index.html s3://${S3_BUCKET}/iOS/index.html --only-show-errors
+aws s3 cp /tmp/index.html s3://${S3_BUCKET}/iOS/latest/index.html --only-show-errors
