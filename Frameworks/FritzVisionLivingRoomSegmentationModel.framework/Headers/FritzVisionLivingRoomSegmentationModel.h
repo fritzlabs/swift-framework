16e22dab51ec9e9c8f8acb472dfd4b88005d7ce7
Chris Kelly <ghop02@gmail.com>
2018-10-03 12:42:11 -0400

    Add people and living room segmentation models

---
+++ Frameworks/FritzVisionLivingRoomSegmentationModel.framework/Headers/FritzVisionLivingRoomSegmentationModel.h
@@ -0,0 +1,21 @@
+//
+//  FritzVisionLivingRoomSegmentationModel.h
+//  FritzVisionLivingRoomSegmentationModel
+//
+//  Created by Christopher Kelly on 9/24/18.
+//  Copyright © 2018 Fritz Labs Incorporated. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+@import FritzVisionSegmentationModel;
+@import FritzManagedModel;
+
+//! Project version number for FritzVisionLivingRoomSegmentationModel.
+FOUNDATION_EXPORT double FritzVisionLivingRoomSegmentationModelVersionNumber;
+
+//! Project version string for FritzVisionLivingRoomSegmentationModel.
+FOUNDATION_EXPORT const unsigned char FritzVisionLivingRoomSegmentationModelVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <FritzVisionLivingRoomSegmentationModel/PublicHeader.h>
+
+
---
+++ Frameworks/FritzVisionPeopleSegmentationModel.framework/Headers/FritzVisionPeopleSegmentationModel.h
@@ -0,0 +1,19 @@
+//
+//  FritzVisionPeopleSegmentationModel.h
+//  FritzVisionPeopleSegmentationModel
+//
+//  Created by Christopher Kelly on 9/24/18.
+//  Copyright © 2018 Fritz Labs Incorporated. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+@import FritzVisionSegmentationModel;
+@import FritzManagedModel;
+
+//! Project version number for FritzVisionPeopleSegmentationModel.
+FOUNDATION_EXPORT double FritzVisionPeopleSegmentationModelVersionNumber;
+
+//! Project version string for FritzVisionPeopleSegmentationModel.
+FOUNDATION_EXPORT const unsigned char FritzVisionPeopleSegmentationModelVersionString[];
+
+
